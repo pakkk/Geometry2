5a916ef76e01c58893f31d58be63817a69ddb89e
Alvarolopmon <alopmon563@g.educaand.es>
2023-03-19 21:48:42 +0100

    Solucion del error del Heptagono, hecho por Alvaro López Montero

--- src/main/java/es/iesjandula/geometry2/Heptagono.java
+++ src/main/java/es/iesjandula/geometry2/Heptagono.java
@@ -1,10 +1,29 @@
 package es.iesjandula.geometry2;
 
-public class Heptagono {
-
-	public static void main(String[] args) {
+public class Heptagono implements IPolygon {
+	
+	
+	public int getNumberOfSides() {
 		// TODO Auto-generated method stub
+		return 7;
+	}
 
+	public double calculatePerimeter() {
+		double lado = 5;
+	
+		return lado * getNumberOfSides() ;
+		//use this funtion for calculate perimeter. 
+		
+	}
+
+	public double calculateArea() {
+		
+		double apotema = 6;
+		
+		
+		
+		return (apotema * calculatePerimeter())/2;
+		//use this funtion for calculate area.
 	}
 
 }
