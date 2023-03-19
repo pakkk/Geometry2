6df69487db60d4dd20209a5d6b09ed7fe6d08973
Veerdejoo 7 <avergar952@g.educaand.es>
2023-03-19 14:10:48 +0100

    Codigo Heptadecagono.java Verdejo

--- src/main/java/es/iesjandula/geometry2/Heptadecagono.java
+++ src/main/java/es/iesjandula/geometry2/Heptadecagono.java
@@ -10,38 +10,27 @@ package es.iesjandula.geometry;
  */
 
 public class Heptadecagono implements IPolygon{
-	/** Attribute - Radio */
+
 	private double longitudLado ;
-	
-	/**
-	 * Constructor of the class
-	 * 
-	 * @param radio with the radio of the circle
-	 */
+
 	public Heptadecagono(double longitudLado)
 	{
 		this.longitudLado = longitudLado;
 	}
 	
-	/**
-	 * @return the sides number. In this case, it is zero
-	 */
+	
 	public int getNumberOfSides()
 	{
 		return 17 ;
 	}
 
-	/**
-	 * @return the perimeter of the circle
-	 */
+	
 	public double calculatePerimeter()
 	{
 		return longitudLado * 17;
 	}
 
-	/**
-	 * @return the area of the circle
-	 */
+	
 	public double calculateArea()
 	{
 		return 22.7355  * Math.pow(longitudLado, 2);
